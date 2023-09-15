16e58cf822a182b3ebec162674e9a0470e4eff33
mush1to <mush1to@outlook.jp>
2023-09-16 08:54:41 +0900

    Adapting to Tunecore links

--- src.js
+++ src.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YTMD opener
 // @namespace    http://ytmdesktop.app/
-// @version      0.1
-// @description  Add a button in youtube music to help you open it in YTMDesktop app
-// @author       You
+// @version      0.1_v6410
+// @description  Add a button in youtube music to help you open it in YTMDesktop app. Adapting to Tunecore links[no list ID].
+// @author       mingjun97, mush1to
 // @match        https://music.youtube.com/*
 // @grant        none
 // ==/UserScript==
@@ -32,7 +32,7 @@
     button.addEventListener('click', function() {
         var videoId = new URLSearchParams(window.location.search).get('v');
         var listId = new URLSearchParams(window.location.search).get('list');
-        if (videoId) window.location = `ytmd://play/${videoId}/${listId}`;
+        if (videoId) window.location = `ytmd://play/${videoId}`;
         stopPlay();
     });
 })();
